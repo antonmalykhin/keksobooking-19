ce946dbe66cd5097fb5eecc6f4bff5b1094c4e35
antonmalykhin <a.y.malykhin@gmail.com>
2020-03-01 22:49:46 +0300

    Добавляте отрисовку только 5 пинов
    Добавляет функцию makeCard для отрисовки карточки по нажатию
    на пин
    Добавляет константу ADVERTS_NUM для определения кол-ва пинов
    для отрисовки

--- js/map.js
+++ js/map.js
@@ -1,9 +1,28 @@
 'use strict';
 
 (function () {
+
+  /**
+   * Количество показываемых пинов
+   * @constant
+   * @type {number}
+   */
+  var ADVERTS_NUM = 5;
+
   var map = document.querySelector('.map');
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var mapPins = map.querySelector('.map__pins');
+
+  /**
+   * Функция отрисовки карточки по нажатию на пин
+   * @param {*} pin - пин
+   * @param {*} advert - объявление
+   */
+  var makeCard = function (pin, advert) {
+    pin.addEventListener('click', function () {
+      window.card.renderCard(advert);
+    });
+  };
 
   /**
    * Функция рисует маркеры объявления из массива объектов объявлений
@@ -11,17 +30,19 @@
    */
   var renderMapPins = function (adverts) {
     var fragment = document.createDocumentFragment();
-    adverts.forEach(function (advert) {
+
+    for (var i = 0; i < ADVERTS_NUM; i++) {
       var pin = pinTemplate.cloneNode(true);
       var avatar = pin.querySelector('img');
+      var advert = adverts[i];
       pin.style = 'left: ' + advert.location.x + 'px; top: ' + advert.location.y + 'px;';
       avatar.src = advert.author.avatar;
       avatar.alt = advert.offer.title;
+
+      makeCard(pin, advert);
+
       fragment.appendChild(pin);
-      pin.addEventListener('click', function () {
-        window.card.renderCard(advert);
-      });
-    });
+    }
     mapPins.appendChild(fragment);
   };
 
