94706530ccfbb6ba7360db7a9e8636ad9668863a
antonmalykhin <a.y.malykhin@gmail.com>
2020-03-16 13:28:55 +0300

    Добавляет сообщения об ошибках Некоторые константы добасляет в перечисления

--- js/backend.js
+++ js/backend.js
@@ -2,10 +2,17 @@
 
 (function () {
 
-  var TIMEOUT = 10000;
-  var URL_LOAD = 'https://js.dump.academy/keksobooking/data';
-  var URL_UPLOAD = 'https://js.dump.academy/keksobooking';
   var RESPONSE_TYPE = 'json';
+
+  var Url = {
+    LOAD: 'https://js.dump.academy/keksobooking/data',
+    UPLOAD: 'https://js.dump.academy/keksobooking'
+  };
+
+  var Timeout = {
+    LOAD: 10000,
+    MESSAGE: 3000
+  };
 
   var ErrorText = {
     STATUS: 'Статус ответа: ',
@@ -17,6 +24,10 @@
   var StatusCode = {
     OK: 200
   };
+
+  var main = document.querySelector('main');
+  var notice = main.querySelector('.notice');
+
 
   var load = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
@@ -39,9 +50,9 @@
       onError(ErrorText.TIMEOUT + xhr.timeout + ErrorText.TIME);
     });
 
-    xhr.timeout = TIMEOUT;
+    xhr.timeout = Timeout.LOAD;
 
-    xhr.open('GET', URL_LOAD);
+    xhr.open('GET', Url.LOAD);
     xhr.send();
   };
 
@@ -49,7 +60,7 @@
   var upload = function (data, onSuccess, onError) {
     var xhr = new XMLHttpRequest();
 
-    xhr.responseType = 'json';
+    xhr.responseType = RESPONSE_TYPE;
 
     xhr.addEventListener('load', function () {
       if (xhr.status === StatusCode.OK) {
@@ -63,13 +74,66 @@
       onError(ErrorText.DISCONNECT);
     });
 
-    xhr.open('POST', URL_UPLOAD);
+    xhr.open('POST', Url.UPLOAD);
     xhr.send(data);
+  };
+
+  var onErrorLoad = function (errorText) {
+    var errorMessageTemplate = document.querySelector('#error').content.querySelector('.error');
+    var errorMessage = errorMessageTemplate.cloneNode();
+    var errorMessageText = document.createElement('p');
+
+    errorMessageText.classList.add('error__message');
+    errorMessageText.innerText = errorText;
+
+    errorMessage.appendChild(errorMessageText);
+
+    main.insertBefore(errorMessage, notice);
+    window.setTimeout(function () {
+      errorMessage.remove();
+      window.page.reset();
+    }, Timeout.MESSAGE);
+
+  };
+
+  var onErrorUpload = function (errorText) {
+    var errorMessageTemplate = document.querySelector('#error').content.querySelector('.error');
+    var errorMessage = errorMessageTemplate.cloneNode(true);
+    var errorMessageText = errorMessage.querySelector('.error__message');
+    errorMessageText.innerText = errorText;
+    var tryAgainBtn = errorMessage.querySelector('.error__button');
+
+    main.insertBefore(errorMessage, notice);
+
+    var closeErrorMessage = function () {
+      errorMessage.remove();
+      tryAgainBtn.removeEventListener('click', onTryAgainBtnClick);
+      document.removeEventListener('keydown', onEscapeKeyPress);
+      document.removeEventListener('click', onEmptyAriaClick);
+    };
+
+    var onTryAgainBtnClick = function () {
+      closeErrorMessage();
+    };
+
+    var onEscapeKeyPress = function () {
+      closeErrorMessage();
+    };
+
+    var onEmptyAriaClick = function () {
+      closeErrorMessage();
+    };
+
+    tryAgainBtn.addEventListener('click', onTryAgainBtnClick);
+    document.addEventListener('keydown', onEscapeKeyPress);
+    document.addEventListener('click', onEmptyAriaClick);
   };
 
   window.backend = {
     load: load,
-    upload: upload
+    upload: upload,
+    onErrorLoad: onErrorLoad,
+    onErrorUpload: onErrorUpload
   };
 
 })();
